4110f7c872b51ee1dd8c6aece88908f55a5d0eb2
Repsay <jdelahaije@gmail.com>
2022-01-15 09:36:39 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,27 +4,27 @@ from setuptools import find_packages, setup
 
 HERE = path.abspath(path.dirname(__file__))
 
-with open(path.join(HERE, 'README.md')) as f:
+with open(path.join(HERE, "README.md")) as f:
     README = f.read()
 
 setup(
-    name='supermarket-connector',
+    name="supermarket-connector",
     packages=find_packages(),
-    version='0.0.1',
-    description='Supermarket Mobile API Connector',
+    version="0.0.2",
+    description="Supermarket Mobile API Connector",
     long_description=README,
-    long_description_content_type='text/markdown',
-    author='Jasper Delahaije',
-    author_email='jdelahaije@gmail.com',
-    license='MIT',
-    python_requires='>=3',
+    long_description_content_type="text/markdown",
+    author="Jasper Delahaije",
+    author_email="jdelahaije@gmail.com",
+    license="MIT",
+    python_requires=">=3",
     install_requires=["requests"],
     include_package_data=True,
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.9'
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
     ],
 )
